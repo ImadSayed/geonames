d146253a09c4c6b745c0f6d29c159831320dc446
Imad Sayed <sayedimad@gmail.com>
2021-01-29 07:57:59 +0000

    add loading... to initial page load

--- js/getCountriesScript.js
+++ js/getCountriesScript.js
@@ -1,28 +1,34 @@
-$.ajax({
-    url: "/weather/php/getCountryList.php",
-    type: 'POST',
-    success: function(result) {
-        
-        if (result.status.name == "ok") {
-            const len =  result['data'].length; //results array length
+$(document).ready(() => {
+    $('#loadingM').css('display','block');
+    $('#mainDiv').css('display','none');
+    $.ajax({
+        url: "/weather/php/getCountryList.php",
+        type: 'POST',
+        success: function(result) {
 
-            function fillDropDownList() {
-                const container = document.getElementById('dd_country');   //html drop down list <select>
-                
-                for(let i = 0; i < len; i++) {
-                    let opt = document.createElement('option'); //create an option for the drop down list
-                    opt.value = result['data'][i]['countryCode']; //assign option value
-                    let text = document.createTextNode(result['data'][i]['countryName']);
-                    opt.appendChild(text);
-                    container.appendChild(opt);
+            if (result.status.name == "ok") {
+                const len =  result['data'].length; //results array length
+
+                function fillDropDownList() {
+                    const container = document.getElementById('dd_country');   //html drop down list <select>
+                    
+                    for(let i = 0; i < len; i++) {
+                        let opt = document.createElement('option'); //create an option for the drop down list
+                        opt.value = result['data'][i]['countryCode']; //assign option value
+                        let text = document.createTextNode(result['data'][i]['countryName']);
+                        opt.appendChild(text);
+                        container.appendChild(opt);
+                    }
+                    $('#loadingM').css('display','none');
+                    $('#mainDiv').css('display','flex');
                 }
+                fillDropDownList();
             }
-            fillDropDownList();
+        
+        },
+        error: function(error) {
+            // your error code
+            console.log("Error - getCountriesScript.js - : "+error.message);
         }
-    
-    },
-    error: function(error) {
-        // your error code
-        console.log("Error - getCountriesScript.js - : "+error.message);
-    }
-}); 
+    }); 
+});
